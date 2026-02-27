323d69209cbcdb505e67f4ca780d9793ac355ec6
Oleh Skibinskyi <oleg.sk2002@gmail.com>
2026-02-27 22:09:08 +0200

    feature: add exceptions && validators

--- src/exceptions/__init__.py
+++ src/exceptions/__init__.py
@@ -1,6 +1,25 @@
-from src.exceptions.token_exceptions import TokenExpiredError, InvalidTokenError
+from src.exceptions.token_exceptions import (
+    TokenExpiredError,
+    InvalidTokenError,
+)
+from src.exceptions.user import (
+    IncorrectPasswordError,
+    IncorrectLoginError,
+    IncorrectCredentials,
+    UserNotExist,
+    UserAlreadyExist,
+    UserGroupNotExist,
+    UserPermissionDenied,
+)
 
 __all__ = [
     "InvalidTokenError",
     "TokenExpiredError",
+    "IncorrectPasswordError",
+    "IncorrectLoginError",
+    "IncorrectCredentials",
+    "UserNotExist",
+    "UserAlreadyExist",
+    "UserGroupNotExist",
+    "UserPermissionDenied",
 ]
