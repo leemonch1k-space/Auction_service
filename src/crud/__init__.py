3ffb072375124e20af3a2625fde736320042e1dd
Oleh Skibinskyi <oleg.sk2002@gmail.com>
2026-02-28 21:32:08 +0200

    feature: add auction crud

--- src/crud/__init__.py
+++ src/crud/__init__.py
@@ -1,7 +1,17 @@
 from src.crud.user import login_user, create_new_user, refresh_token
+from src.crud.auction import (
+    get_user_collection,
+    create_new_auction,
+    create_new_lot_item,
+    get_auctions
+)
 
 __all__ = [
     "login_user",
     "create_new_user",
     "refresh_token",
+    "get_user_collection",
+    "create_new_auction",
+    "create_new_lot_item",
+    "get_auctions",
 ]
