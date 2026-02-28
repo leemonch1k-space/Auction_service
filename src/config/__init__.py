71e5562c19733586b574ccaa868af5412c60d907
Oleh Skibinskyi <oleg.sk2002@gmail.com>
2026-02-28 21:26:02 +0200

    fix: resolved circular import issue

--- src/config/__init__.py
+++ src/config/__init__.py
@@ -1,14 +1,6 @@
-from src.config.dependencies import (
-    get_db,
-    get_jwt_manager,
-    get_authenticated_user
-)
 from src.config.settings import BaseAppSettings, DevSettings, Settings
 
 __all__ = [
-    "get_db",
-    "get_jwt_manager",
-    "get_authenticated_user",
     "BaseAppSettings",
     "DevSettings",
     "Settings",
