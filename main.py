5fe5d0c3b3887982bf68966ef6dc885a7d6724d4
Oleh Skibinskyi <oleg.sk2002@gmail.com>
2026-02-28 21:38:47 +0200

    feature: add main router

---
+++ main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from src.exceptions import BaseUserException
+from src.routers import(
+    auth_router,
+    auction_router
+)
+app = FastAPI()
+
+app.include_router(auth_router)
+app.include_router(auction_router)
+
+@app.exception_handler(BaseUserException)
+async def validation_exception_handler(request, exc):
+    return JSONResponse(
+        status_code=400,
+        content={"Error": "Validation error", "detail": str(exc)},
+    )
